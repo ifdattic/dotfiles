f61434e3dbdbdcad3cdb6ef75276959a7fdfefb3
Mathias Bynens <mathias@qiwi.be>
2016-04-08 07:36:31 +0200

    brew.sh: Remove useless use of `sudo`
    Thanks to @jonnybarnes for spotting this.
    
    Closes #647.

--- brew.sh
+++ brew.sh
@@ -12,12 +12,6 @@ function postUpdatePHP() {
     sed -i -r 's/;?phar.readonly = Off/phar.readonly = On/' /usr/local/etc/php/5.6/php.ini
 }
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Make sure we’re using the latest Homebrew.
 brew update
 
@@ -27,7 +21,7 @@ brew upgrade --all
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
