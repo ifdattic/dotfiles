457d10a3d3101d5080a7d1b03f6967d37819a8c3
Andrew Marcinkevičius <andrew.web@ifdattic.com>
2016-06-19 17:58:00 +0300

    brew.sh: Install Mou

--- brew.sh
+++ brew.sh
@@ -88,6 +88,7 @@ brew install webkit2png
 brew cask install android-file-transfer
 brew cask install bartender
 brew cask install licecap
+brew cask install mou
 brew cask install phpstorm
 brew cask install selfcontrol
 brew cask install sequel-pro
