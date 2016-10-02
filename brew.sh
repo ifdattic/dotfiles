35a845b506d0570f29a0be5fc8b27992bf01bd23
Lipis <lipiridis@gmail.com>
2016-10-02 22:06:09 +0200

    .brew.sh: Remove pointless symlink
    The following is unnecessary:
    
        ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
    
    …because the non-prefixed version is already in `$(brew --prefix coreutils)/libexec/gnubin`.
    
    https://github.com/Homebrew/brew/issues/1171#issuecomment-250906442
    
    Closes #709.

--- brew.sh
+++ brew.sh
@@ -21,7 +21,6 @@ brew upgrade --all
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
