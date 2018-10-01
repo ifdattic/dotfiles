009269197b284aacc0c62bdb63ef195fad77712c
Andrew Marcinkevičius <andrew.web@ifdattic.com>
2018-10-01 14:16:48 +0300

    Respect non-default brew installation locations

--- brew.sh
+++ brew.sh
@@ -18,9 +18,13 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
+# Save Homebrew's installed location.
+BREW_PREFIX=$(brew --prefix)
+
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -38,9 +42,9 @@ brew install bash
 brew install bash-completion
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
 # Install `wget` with IRI support.
